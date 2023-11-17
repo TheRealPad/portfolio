cc97a668c2c21f71ea85db31d3ab575617d40109
Pierre-Alexandre <72022523+TheRealPad@users.noreply.github.com>
2023-11-18 00:02:47 +0100

    fix(cllient ): remove unused variables

--- src/pages/me/timeline/index.tsx
+++ src/pages/me/timeline/index.tsx
@@ -1,16 +1,7 @@
-import { TimelineElement } from "./types.ts";
+
 import styles from "./styles.module.scss";
 
 function Timeline() {
-  const timelineElements: [TimelineElement] = [
-    {
-      name: "Epitech",
-      description: "Best school in the world",
-      type: "SCHOOL",
-      startDate: new Date("2020-10-1"),
-      endDate: new Date("2025-05-1"),
-    },
-  ];
 
   return (
     <div className={styles.timeline}>
